9e9a05216970c42f3f64946ab79c8c837faac5ca
0xsadik <torikussadik404@gmail.com>
2023-11-28 19:30:24 +0600

    u...

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,10 @@ import { navLinks } from '../constants'
 const Navbar = () => {
     return (
         <nav className='w-full flex py-6 justify-between items-center navbar'>
-            <img src={logo} alt="hoobank" className='' />
+            <img src={logo} alt="hoobank" className='w-[124px] h-[32px]' />
+            <ul className=''>
+
+            </ul>
         </nav>
     )
 }
