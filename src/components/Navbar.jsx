4d4706e479a2b6e7c4d1422b84f7091e43a7d852
0xsadik <torikussadik404@gmail.com>
2023-11-28 19:34:45 +0600

    u...

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,8 +9,10 @@ const Navbar = () => {
             <img src={logo} alt="hoobank" className='w-[124px] h-[32px]' />
             <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
                 {navLinks.map((nav, index) =>  (
-                    <li key={nav.id} className={`font-poppins`}>
-                        <a href={`#${nav.id}`}></a>
+                    <li key={nav.id} className={`font-poppins font-normal cursor-pointer`}>
+                        <a href={`#${nav.id}`}>
+                            {nav.title}
+                        </a>
                     </li>
                 ))}
             </ul>
